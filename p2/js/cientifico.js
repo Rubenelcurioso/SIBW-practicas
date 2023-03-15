b4beff7a7016c6baf72ac00323d941a205e54eee
Rubenelcurioso <ruben.rosales.tapia@gmail.com>
2023-03-15 18:13:41 +0100

    Function to toggle comments

--- p2/js/cientifico.js
+++ p2/js/cientifico.js
@@ -1,2 +1,13 @@
 const botonComentario = document.getElementById("bComm");
 
+function mostrarComentarios(){
+    var seccionComments = document.getElementById("comentarios");
+    if(seccionComments.style.display=="none"){
+        seccionComments.style.display = "block";
+        return false;
+    }
+    seccionComments.style.display = "none";
+    return true;
+}
+
+botonComentario.addEventListener("click",mostrarComentarios);
